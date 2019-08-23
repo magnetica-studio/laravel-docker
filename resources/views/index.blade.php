b475422cc21b6ea975088d332a58561351872d2b
szk2s <suzukisjss@gmail.com>
2019-08-23 19:43:42 +0900

    feat($view): impl landing page

---
+++ resources/views/index.blade.php
@@ -0,0 +1,15 @@
+<html>
+   <body>
+        <h1>
+            Welcome to laravel world!
+        </h1>
+        <ul>
+            <li>
+                <a href="users">User List</a>
+            </li>
+            <li>
+                <a href="posts">Posts</a>
+            </li>
+        </ul>
+   </body>
+</html>
